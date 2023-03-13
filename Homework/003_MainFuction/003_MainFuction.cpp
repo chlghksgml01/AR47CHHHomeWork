5125fbe0b66ace873d91e4cf416d40b2d103c447
chlghksgml01 <chlghksgml01@naver.com>
2023-03-13 10:23:46 +0900

    최환희

---
+++ Homework/003_MainFuction/003_MainFuction.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+
+int main()
+{
+	int left = 7;
+	int right = 3;
+	int result = 0;
+	result = left + right;
+	result = left - right;
+	result = left * right;
+	result = left / right;
+	result = left % right;
+	result = +left;
+	result = -left;
+	result = ++left;
+	result = --left;
+	bool bresult = true;	
+	int value = 0b01111111111111111111111111111111;
+	value = 0b11111111111111111111111111111111;
+	value = 0b11111111111111111111111111111110;
+	value = 0b11111111111111111111111111111100;
+	value = 0b11111111111111111111111111111011;
+}
