4b8c7b1d9c86755a0d9f3b21ba7684dc244f3d3e
chlghksgml01 <chlghksgml01@naver.com>
2023-03-14 15:50:51 +0900

    최환희:0314

---
+++ Homework/0314_array/0314_array.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+
+int main()
+{
+	{
+		// 어떤 자료형을 봤다면 추가적인 문법으로 모두 다 배열이 될 수 있음
+		int value = 10;
+
+		// 배열은 괄호 안에 아무것도 안넣으면 자동으로 전부 다 0으로 초기화됨
+		int Arrvalue[5] = {}; // Arrvalue : {0, 0, 0, 0, 0}
+		// 맨 앞만 1, 나머지 다 0
+		int Arrvalue2[5] = { 1, }; // Arrvalue2 : {1, 0, 0, 0, 0}
+
+		// bool 배열 쓸때는 false, true로 넣기
+		bool Arrvalue3[5] = { false, true, }; // Arrvalue3 : {false, true, false, false, false};
+
+		// 문자의 배열은 축약형 표현 사용할 수 X 문법적으로 정해짐
+	}
+	{
+		char Arrvalue[5] = { 'a', 'b', 'c' };
+		char Arrvalue2[5] = "abc";
+	}
+}
---
+++ Homework/0314_inline/0314_inline.cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>
+
+// inline : 함수 안의 내용을 main 안에 복붙 -> 스택영역에 Test 자리 안만들어도 됨 -> 최적화할 수 있다면 해달라는 코드
+// 컴파일러 판단에 따라 안될수도 있음
+inline void Test()
+{
+    printf_s("Function");
+}
+int main()
+{
+    Test();
+}
